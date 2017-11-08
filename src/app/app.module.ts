d5e84dd90aa9cb5e7994a0462dc46175ca679181
Akshata Mohanty <akshatamohanty@gmail.com>
2017-11-08 17:55:24 +0930

    test commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CanvasComponent } from './canvas/canvas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanvasComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule
   ],
   providers: [],
